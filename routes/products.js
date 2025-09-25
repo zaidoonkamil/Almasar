24feaa8bc33ea1f8647b6342096d861932a8a424
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-09-25 17:43:33 +0300

    .

--- routes/products.js
+++ routes/products.js
@@ -34,7 +34,7 @@ router.get("/vendor/:vendorId/products", async (req, res) => {
   try {
     const { vendorId } = req.params;
     const page = parseInt(req.query.page) || 1; 
-    const limit = parseInt(req.query.limit) || 10; 
+    const limit = parseInt(req.query.limit) || 60; 
     const offset = (page - 1) * limit;
 
     const { count, rows } = await Product.findAndCountAll({
