0cdda6b12d6dd4e0c2070aaa6a6f1a00d0bf173b
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-01-17 16:25:29 +0300

    .

--- routes/admin.js
+++ routes/admin.js
@@ -105,15 +105,8 @@ router.get("/admin/returned-or-exchanged-orders", async (req, res) => {
 
 router.get("/admin/order-pending", async (req, res) => {
   try {
-    await Order.destroy({
-      where: {
-        [Op.or]: [
-          { vendorId: null },
-          { productId: null }
-        ]
-      }
-    });
-    
+   
+
     const orders = await Order.findAll({
       where: {
         status: "تم الاستلام",
