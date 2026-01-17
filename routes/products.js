31f3c3f3c38dba3b2016be73816d6f60e7eeb1ab
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2026-01-17 15:37:13 +0300

    .

--- routes/products.js
+++ routes/products.js
@@ -117,7 +117,7 @@ router.delete("/vendor/:vendorId/products/:productId", async (req, res) => {
 router.get("/vendorbysponsored", async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 90;
+    const limit = parseInt(req.query.limit) || 10;
     const offset = (page - 1) * limit;
 
     // التجار الموصى بهم (اللي عندهم قيمة > 0)
